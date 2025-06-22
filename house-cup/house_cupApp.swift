2f8376288bd009a53d3fd857acf9786c5612206a
Jack Gaffney <jackgaff@umich.edu>
2025-06-21 20:33:37 -0400

    Initial Commit

---
+++ house-cup/house_cupApp.swift
@@ -0,0 +1,17 @@
+//
+//  house_cupApp.swift
+//  house-cup
+//
+//  Created by Jack Gaffney on 6/21/25.
+//
+
+import SwiftUI
+
+@main
+struct house_cupApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ house-cupTests/house_cupTests.swift
@@ -0,0 +1,17 @@
+//
+//  house_cupTests.swift
+//  house-cupTests
+//
+//  Created by Jack Gaffney on 6/21/25.
+//
+
+import Testing
+@testable import house_cup
+
+struct house_cupTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
